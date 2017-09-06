4590ba58cc84ac10eb29f30069afa5077de917c0
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-09-06 16:47:07 +0200

    Add Custom Search Bar + List View

--- app/src/main/java/com/ezteam/ezlyrics/MainActivity.java
+++ app/src/main/java/com/ezteam/ezlyrics/MainActivity.java
@@ -1,13 +1,112 @@
 package com.ezteam.ezlyrics;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialSearchView searchView;
+    ListView listView;
+
+    String[] listSource = {
+            "Peltou",
+            "Boehm",
+            "On",
+            "Remplit",
+            "La",
+            "Liste",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        listView = (ListView)findViewById(R.id.request_result);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listSource);
+        listView.setAdapter(adapter);
+
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+                //If closed Search View, listView will return default
+
+                listView = (ListView)findViewById(R.id.request_result);
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listSource);
+                listView.setAdapter(adapter);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText != null && !newText.isEmpty()) {
+                    List<String> resultFound = new ArrayList<String>();
+                    for (String item:listSource) {
+                        if (item.contains(newText))
+                            resultFound.add(item);
+                    }
+
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, resultFound);
+                    listView.setAdapter(adapter);
+                }
+                else {
+
+                    //If search text is null
+                    //return default
+
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listSource);
+                    listView.setAdapter(adapter);
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
     }
 }
